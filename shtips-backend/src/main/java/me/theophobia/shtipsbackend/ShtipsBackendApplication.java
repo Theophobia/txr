0f9f74e57572fbcc2552a118a9e93d497dcadee3
Theophobia <taekwondo1337@gmail.com>
2023-06-23 23:32:14 +0300

    yees

---
+++ shtips-backend/src/main/java/me/theophobia/shtipsbackend/ShtipsBackendApplication.java
@@ -0,0 +1,15 @@
+package me.theophobia.shtipsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShtipsBackendApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+		System.out.println("Hello, world!");
+		SpringApplication.run(ShtipsBackendApplication.class, args);
+	}
+
+}
---
+++ shtips-backend/src/test/java/me/theophobia/shtipsbackend/ShtipsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.theophobia.shtipsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShtipsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
